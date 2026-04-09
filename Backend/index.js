292c17c042b87ee3b9a7027d991df37de2a00ff4
RanjanaDevi1104 <ranju94644@gmail.com>
2026-04-09 10:34:59 +0530

    new data add

--- Backend/index.js
+++ Backend/index.js
@@ -1,29 +1,38 @@
-
-
 import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
-import router from './Router/LoginRouter.js'
-import router2 from './Router/imageRouter.js'
-import AudioRoute from './Router/AudioRoute.js'
+
+import router from './Router/LoginRouter.js';
+import router2 from './Router/imageRouter.js';
+import AudioRoute from './Router/AudioRoute.js';
 
 const app = express();
+
 app.use(express.json());
+
 app.use(cors({
-  origin: "https://deluxe-sunflower-8b33ad.netlify.app", // Ya apna frontend URL
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "token"] 
+  origin: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "token", "Authorization"],
+  credentials: true
 }));
-app.use("/api",router)
-app.use("/api",router2)
-app.use("/api/audio",AudioRoute)
 
- mongoose
- .connect(process.env.MONGO_URL)
-.then(()=>console.log("db connected"))
-.catch((err)=>console.log(err))
+app.get("/", (req, res) => {
+  res.send("Backend is running successfully");
+});
 
-app.listen(process.env.PORT)
+app.use("/api", router);
+app.use("/api", router2);
+app.use("/api/audio", AudioRoute);
+
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log("db connected"))
+  .catch((err) => console.log(err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
